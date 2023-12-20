61f545a1cc25e81a6cbe3d79c3dfb74df9093b51
petardaszymek <130040535+petardaszymek@users.noreply.github.com>
2023-12-21 00:22:39 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from src.functions import report
+import argparse
+from functions import report
 
 
 def main(file_name, output_path=".", year=None, month=None):
@@ -6,4 +7,14 @@ def main(file_name, output_path=".", year=None, month=None):
 
 
 if __name__ == '__main__':
-    main(file_name="csv2020", output_path="C:/Users/User/Desktop", year=2020)
+    parser = argparse.ArgumentParser(description='Process some command-line arguments.')
+
+    parser.add_argument('--file_name', type=str, help='Specify the file name')
+    parser.add_argument('--output_path', type=str, default='.',
+                        help='Specify the output path (default: current directory)')
+    parser.add_argument('--year', type=int, help='Specify the year')
+    parser.add_argument('--month', type=int, help='Specify the month')
+
+    args = parser.parse_args()
+
+    main(file_name=args.file_name, output_path=args.output_path, year=args.year, month=args.month)
